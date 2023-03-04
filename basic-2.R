a4c6753ad6e067a52e6689296a678f8c6f39bcf0
LalluPrathapan <lalsdestiny@gmail.com>
2023-03-04 22:31:46 +0100

    update

--- basic-2.R
+++ basic-2.R
@@ -8,7 +8,7 @@
 # Importing of shapefile into R is using the readOGR() function of sp package
 #path is the first parameter and the name of the shapefile is second
 
-
+library(sf)
 #Spatial points
 library(sp)
 library(rgdal)
@@ -58,3 +58,16 @@ str(map_dhaka,max.level = 2)
 #2coorners of the bounding box
 #totatl of 5slots and each can be accessed using @
 
+
+#Import another database of shapefile direclty from github using Sf package
+# we can use st_read for this
+#You can use GDAL Virtual File Systems with sf::st_read(), 
+#the one for reading files over HTTP & FTP is /vsicurl/ and 
+#apparently it's capable enough to figure out which additional files it should pull to make Shapefiles work:
+shp=st_read("/vsicurl/http://wesleysc352.github.io/seg_s3_r3_m10_fix_estat_amost_val.shp")
+
+
+#however the datafile we need couldn't be downloade so ???
+map_bd=readOGR("bd_adm","BGD_adm3_data_re")
+head(map_bd)
+str(map_bd,max.level = 2)
